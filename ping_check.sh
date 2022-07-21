07e87ab4e26ddaea67dd1f5808a2a0bcbd921fbc
AHazimy <aly.hazimy.95@gmail.com>
2022-07-21 23:51:36 +0300

    Edit Ping_Check

--- ping_check.sh
+++ ping_check.sh
@@ -5,8 +5,16 @@ echo "You forgot an IP address"
 echo "Syntax: ./ipsweep.sh 192.168.1"
 
 else
+counter=0
+ping_value=""
 for ip in `seq 1 254`; do
-ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" 
+ping_value=$(ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":")
+if [[ "$ping_value" -ne "" ]] 
+then
+((counter++))
+echo $counter
+echo $ping_value
+fi
 done
 fi
 
