9e2302d21e6bb761167a4cf70d0b3ed55b49a4ac
AHazimy <aly.hazimy.95@gmail.com>
2022-07-24 20:55:55 +0300

    Editing

--- ping_check.sh
+++ ping_check.sh
@@ -7,9 +7,11 @@ echo "Please enter like './ping_check.sh X.X.X'"
 else
 counter=0
 ping_value=""
-for ip in `seq 1 254`; do
+for ip in `seq 1 254`; 
+do
 ping_value=$(ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":")
-if [[ "$ping_value" -ne "" ]] 
+
+if [[ "$ping_value" != "" ]] 
 then
 ((counter++))
 echo $counter
